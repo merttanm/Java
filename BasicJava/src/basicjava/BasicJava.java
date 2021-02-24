31cc57753449a259a26331210de2b71361b56816
merttanm <68250572+merttanm@users.noreply.github.com>
2021-02-24 21:45:31 +0300

    Chapter 1
    Sınıf oluşturma ve değişkenler

--- BasicJava/src/basicjava/BasicJava.java
+++ BasicJava/src/basicjava/BasicJava.java
@@ -4,18 +4,30 @@
  * and open the template in the editor.
  */
 package basicjava;
-
+import java.awt.Graphics;
 /**
  *
  * @author MERT
  */
-public class BasicJava {
+public class BasicJava extends java.applet.Applet{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       // Değişken tanımlama
+       int number1=23;
+       int number2=44;
+       int number3=number1+number2;
+       String name="Mert tan";
+       System.out.println("Sayi1: "+ number1 + " İsim: " + name);
+       System.out.println("Sayi3: " + number3);
+       System.out.println("Yeni sayı "+number1++);
+       System.out.println("İsim uzunluğu "+name.length());
+        
+       Integer sayi2=new Integer(46);
+       System.out.println("Sayi2: " +sayi2); 
+       
+       
     }
-    
 }
