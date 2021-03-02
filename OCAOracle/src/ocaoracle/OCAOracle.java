b1b61aea2a4949c82a68df6af62a506f79d853ea
merttanm <68250572+merttanm@users.noreply.github.com>
2021-03-03 00:43:52 +0300

    Oracle sertifikasyon sınavı

--- OCAOracle/src/ocaoracle/OCAOracle.java
+++ OCAOracle/src/ocaoracle/OCAOracle.java
@@ -15,7 +15,12 @@ public class OCAOracle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        
+        
+        
+        
+        
     }
     
 }
---
+++ OCAOracle/src/ocaoracle/_C.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ocaoracle;
+
+/**
+ *
+ * @author MERT
+ */
+public class _C {
+
+    private static int $;                          // Hata vermez
+
+    public static void main(String[] args) {
+        String a_b;                                // Hata vermez     
+        System.out.println($);                     // Hata vermez
+     // System.out.println(a_b);                   // Hata verir 
+
+    //Hata vermesinin sebebi a_b nin değer atamaması. Bize değer gir yada null gir diyor
+
+    }
+
+}
