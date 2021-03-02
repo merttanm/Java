51cfcd860ca88b91a24b6697dc71e18d0b15e3a1
merttanm <68250572+merttanm@users.noreply.github.com>
2021-03-03 00:42:17 +0300

    Bölüm 4: Tipler ve Değişkenler

---
+++ VariableAndType/src/variableandtype/VariableAndType.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variableandtype;
+
+/**
+ *
+ * @author MERT
+ */
+public class VariableAndType {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //İlkel veri tipleri
+        boolean flag = false;
+        boolean creat = true;
+
+        char firstLetter = 'a'; // Negatif almazz!!
+        char c;
+
+        //Escapes (Kaçış karakterleri)
+        System.out.println("Naber\nBebeğim");
+        System.out.println("Selam\tYavrum");
+        System.out.println("Fatih\bSultan");
+        System.out.println("Pelkas\f\fAlex");
+        System.out.println("Erzinca\nlı");
+        System.out.println("ÖlürümTürkğ\\iyem");
+
+    }
+
+}
