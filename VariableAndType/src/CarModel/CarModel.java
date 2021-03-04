c241150fb6babbc1646d38b87e466ee5e95a4e1f
merttanm <68250572+merttanm@users.noreply.github.com>
2021-03-04 23:29:11 +0300

    Bölüm 4: Tipler ve Değişkenler

--- VariableAndType/src/CarModel/CarModel.java
+++ VariableAndType/src/CarModel/CarModel.java
@@ -10,20 +10,25 @@ package CarModel;
  * @author MERT
  */
 public class CarModel {
-    String marka;
-    String model;
-    String renk;
-    String tipi;
-    String sınıfı;
-    Double kilometre;
-    Double hızlanmaKatsayisi;
+    public String marka;
+    public String model;
+    public String renk;
+    public String tipi;
+    public String sınıfı;
+    public int kilometre;
+    public Double hızlanmaKatsayisi;
     
     public String bilgileri(){
     String bilgileri="Marka:"+marka+ "\n" +"Model:" + model+ "\n"+ "Renk:" + renk +"\n"+"Tipi:"+tipi+"\n"+"Sınıfı:"+sınıfı+"\n"+"Kilometresi:"+kilometre+"\n"+"0-100 hizlanması:"+hızlanmaKatsayisi;
     return bilgileri;
     }
     
-    
+    public void km(int newKilometre){
+    kilometre += newKilometre ;
+        
+    }
+
+   
     
     
     
--- VariableAndType/src/CarModel/CarTest.java
+++ VariableAndType/src/CarModel/CarTest.java
@@ -10,30 +10,32 @@ package CarModel;
  * @author MERT
  */
 public class CarTest {
-    
+
     public static void main(String[] args) {
-        CarModel car1= new CarModel();
-        car1.marka="BMW";
-        car1.model="320";
-        car1.renk="Bordo";
-        car1.sınıfı="A class";
-        car1.tipi="Coupe";
-        car1.hızlanmaKatsayisi=1.4;
-        car1.kilometre=10000.0;
+        CarModel car1 = new CarModel();
+        car1.marka = "BMW";
+        car1.model = "320";
+        car1.renk = "Bordo";
+        car1.sınıfı = "A class";
+        car1.tipi = "Coupe";
+        car1.hızlanmaKatsayisi = 1.4;
+        car1.kilometre = 10000;
         car1.bilgileri();
-        System.out.println( car1.bilgileri());
+        car1.km(33000);
+        System.out.println(car1.bilgileri());
+
         System.out.println("---------------------");
-        CarModel car2= new CarModel();
-        car2.marka="Mercedes";
-        car2.model="cla";
-        car2.renk="Siyah";
-        car2.sınıfı="A class";
-        car2.tipi="Coupe";
-        car2.hızlanmaKatsayisi=1.3;
-        car2.kilometre=20000.0;
+
+        CarModel car2 = new CarModel();
+        car2.marka = "Mercedes";
+        car2.model = "cla";
+        car2.renk = "Siyah";
+        car2.sınıfı = "A class";
+        car2.tipi = "Coupe";
+        car2.hızlanmaKatsayisi = 1.3;
+        car2.kilometre = 20000;
         car2.bilgileri();
-        System.out.println( car2.bilgileri());
+        System.out.println(car2.bilgileri());
     }
-    
-    
+
 }
