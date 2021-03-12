a772e90c9a7a03b31d12f83ae5623b328960e803
merttanm <68250572+merttanm@users.noreply.github.com>
2021-03-12 23:15:22 +0300

    Bölüm 7:Diziler

---
+++ Odevler/src/odevler/DiziyeEkle.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package odevler;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MERT
+ */
+public class DiziyeEkle {
+    public static void main(String[] args) {
+        SieveofErosthenes soe = new SieveofErosthenes();
+        System.out.println("Lütfen bir sayı giriniz");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int array[] = new int[5];
+        
+        for (int i = 0; i < 5; i++) {
+            array[i]= scn.nextInt();
+        }
+        System.out.println("\n");
+        
+        for (int j = 0; j < array.length; j++) {
+            System.out.println(array[j]);
+            
+            
+        }
+    }
+    
+}
---
+++ Odevler/src/odevler/Odevler.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package odevler;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MERT
+ */
+public class Odevler {
+
+    static int array[] = {3, 5, 3, 5, 5, 2, 43, 2};
+    static int arraytekli[] = new int[10];
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            count++;
+            for (int j = 0; j <= i; j++) {
+                if (arraytekli[j] != array[i] && arraytekli[j + 1] != array[i] && arraytekli[j + 2] != array[i]) {
+                    arraytekli[i] = array[i];
+                } else {
+                    j++;
+                    break;
+                }
+
+            }
+
+        }
+
+        for (int i = 0; i < arraytekli.length; i++) {
+            System.out.print(arraytekli[i] + " ");
+        }
+
+    }
+
+}
