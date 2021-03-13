1b2297540b187db7335fb1deb1a9e7b2c685d339
merttanm <68250572+merttanm@users.noreply.github.com>
2021-03-13 21:47:54 +0300

    Oracle sertifikasyon sınavı

---
+++ OCAOracle/src/ThinkingInJava/Object/Object.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ThinkingInJava.Object;
+
+/**
+ *
+ * @author MERT
+ */
+public class Object {
+
+    public static void main(String[] args) {
+        // Character ch = new Character(c);
+        Character ch1 = new Character('a');
+        Character ch2 = 'a';
+        char c = ch2;
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(c);
+
+        String a = new String("Selam");
+    
+  
+    }
+
+}
+
+class ATypeName {
+
+    ATypeName a = new ATypeName();
+   
+
+}
