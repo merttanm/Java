6cfe9760ac22fa563ce2e32db769766f34821639
merttanm <68250572+merttanm@users.noreply.github.com>
2021-05-21 23:28:22 +0300

    Design Policies

---
+++ DesignPolicies/src/Example2/MainTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Example2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author MERT
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    class Car {
+
+        ArrayList<Tire> tire;
+        Engine motor;
+    }
+
+    class Tire {
+    }
+
+    class Engine {
+    }
+
+}
