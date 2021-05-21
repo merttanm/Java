7170cd543cd407ff4e7925763c00ff1442b15c0a
merttanm <68250572+merttanm@users.noreply.github.com>
2021-05-21 23:40:56 +0300

    Design Policies

---
+++ DesignPolicies/src/Example2/ExampleTest3.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Example2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author MERT
+ */
+public class ExampleTest3 {
+
+    public static void main(String[] args) {
+        Documet d= new Documet();
+        d.addPage(new Page());
+        d.addPage(new Page());
+        d.showPages();
+
+    }
+
+    static class Page {
+
+        void render() {
+            System.out.println("Render");
+        }
+    }
+
+    static class Documet {
+
+        ArrayList<Page> pages = new ArrayList();
+
+        void addPage(Page p) {
+            pages.add(p);
+        }
+
+        void showPages() {
+            for (Page page : pages) {
+                page.render();
+
+            }
+        }
+    }
+}
