12b683a74e0b80299851e8f7a78806839b0db0b9
merttanm <68250572+merttanm@users.noreply.github.com>
2021-05-22 23:28:13 +0300

    Design Policies

--- DesignPolicies/src/Example2/Example4.java
+++ DesignPolicies/src/Example2/Example4.java
@@ -5,10 +5,53 @@
  */
 package Example2;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author MERT
  */
 public class Example4 {
     
+    interface IShape{
+    int area();
+    }
+
+    static class Circle  implements IShape{
+
+        int p, r;
+
+        public int area() {
+            return p * r * r;
+
+        }
+    }
+
+    static class Square  implements IShape{
+
+        int r;
+
+        public int area() {
+            return r * r;
+        }
+
+    }
+
+    static class Screen {
+
+        ArrayList<Object> object = new ArrayList();
+
+        int calculateArea() {
+            int totalArea = 0;
+            for (Object object : object) {
+                if (object instanceof Circle) {
+                    totalArea += ((Circle) object).area();
+                } else if (object instanceof Square) {
+                     totalArea += ((Square) object).area();
+                }
+            }
+            return 0;
+        }
+
+    }
 }
