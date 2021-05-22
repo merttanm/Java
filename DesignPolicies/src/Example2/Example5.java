b401c8aeb14961b90b0eeedcd20789344b3b7146
merttanm <68250572+merttanm@users.noreply.github.com>
2021-05-22 23:37:57 +0300

    Design Policies

--- DesignPolicies/src/Example2/Example5.java
+++ DesignPolicies/src/Example2/Example5.java
@@ -10,5 +10,35 @@ package Example2;
  * @author MERT
  */
 public class Example5 {
-    
+
+    static class Bird {
+    }
+
+    static class FlyBird extends Bird {
+
+        void fly() {
+        }
+    }
+
+    static class Duck extends FlyBird {
+    }
+
+    static class NonFlyBird extends Bird {
+
+        void fly() {
+        }
+    }
+
+    static class Penguin extends Bird {
+    }
+
+    public static void main(String[] args) {
+
+        Duck d = new Duck();
+        d.fly();
+        Penguin p = new Penguin();
+      //  p.fly();   Problem
+
+    }
+
 }
