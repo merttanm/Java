349e9e0c6c7c8ad09d77ce699674e3c23a7c6999
merttanm <68250572+merttanm@users.noreply.github.com>
2021-06-10 16:24:06 +0300

    Design Pattern

--- Design-Patterns/src/SingletonExampleBasic/Singleton.java
+++ Design-Patterns/src/SingletonExampleBasic/Singleton.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package SingletonExample;
+package SingletonExampleBasic;
 
 /**
  *
@@ -27,5 +27,7 @@ public class Singleton {
     public void showMessage(){
         System.out.println("Creat Singleton Object...");
     }
+    
+
     
 }
---
+++ Design-Patterns/src/SingletonExampleBookStore/Library.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SingletonExampleBookStore;
+
+/**
+ *
+ * @author MERT
+ */
+public class Library {
+    
+    private boolean receive;
+    private String book;
+    private String students;
+    private static Library uniqueInstance;
+
+    
+    
+    
+    
+    
+}
