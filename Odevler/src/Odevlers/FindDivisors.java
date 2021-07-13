22036fb09c8b96f3afb2bfea89b91ade86b30976
merttanm <68250572+merttanm@users.noreply.github.com>
2021-07-14 00:27:45 +0300

    java practice

--- Odevler/src/Odevlers/FindDivisors.java
+++ Odevler/src/Odevlers/FindDivisors.java
@@ -17,32 +17,72 @@ public class FindDivisors {
 
         Scanner scn = new Scanner(System.in);
         int number = scn.nextInt();
+        int tuttugum = number;
         int countNumber2 = 0;
         int countNumber3 = 0;
+        int countNumber5 = 0;
 
         if (number % 2 == 0) {
             countNumber2++;
             number = number / 2;
-
+            if (number == 1) {
+                System.out.println(tuttugum + " = " + "2^" + countNumber2);
+            }
             if (number % 2 == 0) {
                 countNumber2++;
                 number = number / 2;
-                if (number % 2 == 0) {
+                if (number == 1) {
+                    System.out.println(tuttugum + " = " + "2^" + countNumber2);
+                }
+                if (number % 2 == 0 && number != 1) {
                     countNumber2++;
-                    number = number / 2;
+                    System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3);
+                    if (number % 2 == 0 && number != 2) {
+                        countNumber2++;
+                        System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3);
+                        if (number % 2 == 0 && number != 4) {
+                            countNumber2++;
+                            System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3);
 
+                        } else if (number % 3 == 0) {
+                            countNumber3++;
+                            number = number / 2;
+                            System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3);
+                        }
+                    } else if (number % 3 == 0) {
+                        countNumber3++;
+                        number = number / 2;
+                        System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3);
+                    }
                 } else if (number % 3 == 0) {
                     countNumber3++;
                     number = number / 2;
-                    System.out.println("2^" + countNumber2 + "*" + "3^" + countNumber3);
+                    System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3);
                 }
+                else if (number % 5 == 0) {
+                countNumber5++;
+                number = number / 5;
+                System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3 + " * " + "5^" + countNumber5);
+            }
 
             } else if (number % 3 == 0) {
                 countNumber3++;
-                number = number / 2;
-                System.out.println("2^" + countNumber2 + "*" + "3^" + countNumber3);
+                number = number / 3;
+                System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3);
+            } else if (number % 5 == 0) {
+                countNumber5++;
+                number = number / 5;
+                System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3 + " * " + "5^" + countNumber5);
             }
 
+        } else if (number % 3 == 0) {
+            countNumber3++;
+            number = number / 3;
+            System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3);
+        } else if (number % 5 == 0) {
+            countNumber5++;
+            number = number / 5;
+            System.out.println(tuttugum + " = " + "2^" + countNumber2 + " * " + "3^" + countNumber3 + " * " + "5^" + countNumber5);
         }
 
     }
