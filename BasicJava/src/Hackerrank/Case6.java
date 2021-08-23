eddfc4240f3afbe7ed8e5c5710c8379c53441a70
merttanm <68250572+merttanm@users.noreply.github.com>
2021-08-23 22:43:12 +0300

    HackerRank test

---
+++ BasicJava/src/Hackerrank/Case6.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hackerrank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author merttan
+ */
+public class Case6 {
+    
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner(System.in);
+        int N= scn.nextInt();
+        int result;
+        
+        if(N>=2 && N<=20){
+        for (int i = 1; i <= 10; i++) {
+            result=N*i;
+            System.out.println(N+"*"+i+"="+result );
+        }
+    }
+    
+    }
+    
+}
