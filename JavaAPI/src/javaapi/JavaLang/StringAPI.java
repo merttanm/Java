a830812b7e54df8b909f65653d935ab5d029acbb
merttanm <68250572+merttanm@users.noreply.github.com>
2021-09-07 20:47:09 +0300

    Java.lang packet and String class

--- JavaAPI/src/javaapi/JavaLang/StringAPI.java
+++ JavaAPI/src/javaapi/JavaLang/StringAPI.java
@@ -3,17 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapi.String;
+package javaapi.JavaLang;
+
+import java.util.StringTokenizer;
 
 /**
  *
  * @author merttan
  */
 public class StringAPI {
+    
 
     public static void main(String[] args) {
 
         String kelime = "Selamın Aleyküm";
+        String compareToObject = "Selamın Aleyküm";
+        char kelimeTest[] = new char[10]; //GetChars[] 
+        kelime.getChars(0, 10, kelimeTest, 0); // İlk parametre kopyalanacak 1. karakter 2. parametre kopyalanacak son karakter 3. parametre array son parametre arrayin başlangıç değeri
 
         System.out.println(kelime);
         System.out.println("Kelimenin uzunluğu: " + kelime.length());
@@ -23,15 +29,17 @@ public class StringAPI {
         System.out.println("İndexler arasındaki unikodları sayısı: " + kelime.codePointCount(0, 15));
         System.out.println(kelime.offsetByCodePoints(0, 7));
         System.out.println(kelime.getBytes());
-        char kelimeTest[] = new char[10]; //GetChars[] 
-        kelime.getChars(0, 10, kelimeTest, 0); // İlk parametre kopyalanacak 1. karakter 2. parametre kopyalanacak son karakter 3. parametre array son parametre arrayin başlangıç değeri
         System.out.println(kelimeTest);
         System.out.println(kelime.indexOf(0, 4));
         System.out.println(kelime.lastIndexOf(kelime));
-        System.out.println("Seçilen indexi güncelleme: "+kelime.replace('S','8')); 
-      
-        
-        
+        System.out.println("Seçilen indexi güncelleme: " + kelime.replace('S', '8'));
+        System.out.println("CompareTo: "+ kelime.compareTo(compareToObject)); // ASCI kodlarını mukayese ederek 2 parametrenin eşit olup olmadığını döndürür eşit ise return değeri 0 dır
+        System.out.println("Cümleyi kelimelere ayırırsak ...");
+        StringTokenizer kelimeTolken = new StringTokenizer(kelime);
+        while (kelimeTolken.hasMoreElements()) {
+            System.out.println(kelimeTolken.nextToken());
+        }
+       
 
     }
 
