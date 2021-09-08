95e95fbe436a519d601817806d56c86f8c60b789
merttanm <68250572+merttanm@users.noreply.github.com>
2021-09-08 23:10:44 +0300

    Basic Java

---
+++ BasicJava/src/Hackerrank/Case7.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hackerrank;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author merttan
+ */
+public class Case7 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        String world[]=s.trim().split("[asdf");
+        for(String word: world){
+            System.out.println(word);
+        
+        }
+        
+       /* StringTokenizer kelimeTolken = new StringTokenizer(s);
+
+        while (kelimeTolken.hasMoreElements()) {
+
+            System.out.println(kelimeTolken.nextToken());
+        }
+        scan.close();*/
+   
+    }
+}
