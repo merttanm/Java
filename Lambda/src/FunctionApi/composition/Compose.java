91ad993d802dfd01aac861333d307f97181179e0
merttanm <68250572+merttanm@users.noreply.github.com>
2021-10-28 20:57:30 +0300

    Add files via upload

--- Lambda/src/FunctionApi/composition/Compose.java
+++ Lambda/src/FunctionApi/composition/Compose.java
@@ -9,7 +9,7 @@ package FunctionApi.composition;
  *
  * @author merttan
  */
-public class ComposeFun {
+public class Compose {
 
     //Kendisine verilen parametre ile belirtilen işlemi yapan ve işlemin sonunda belirtilen tipde değer döner
     public interface Fun<A, B> {
@@ -28,9 +28,9 @@ public class ComposeFun {
     }
 
     public static void main(String[] args) {
-        // Yukarıda ki oluşturulan Fun interface i implement ediyoruz
+
         Fun<Double, Double> sin = new Fun<Double, Double>() {
-            @Override
+            @Override     // Yukarıda ki oluşturulan Fun interface' i implement ediyoruz
             public Double call(Double x) {
                 return Math.sin(x);
             }
@@ -38,11 +38,23 @@ public class ComposeFun {
         };
 
         Fun<Double, Double> cos = new Fun<Double, Double>() {
-            @Override
+            @Override     // Yukarıda ki oluşturulan Fun interface' i implement ediyoruz
             public Double call(Double x) {
                 return Math.cos(x);
             }
         };
+
+        System.out.println(cos.call(0.55));  // 0.6 radian 34,37 degrees
+        System.out.println(sin.call(1.1)); // 1.2 radian 68,75 degrees
+
+        Fun<Double, Double> sin1 = (Double degree) -> Math.sin(degree);
+        Fun<Double, Double> asin1 = (Double result) -> Math.asin(result);
+
+        Fun<Double, Double> cosAsin1 = compose(sin, cos);
+        Fun<Double, Double> sinAsin2 = compose(sin1, asin1);
+
+        System.out.println(cosAsin1.call(0.55));
+        System.out.println(sinAsin2.call(0.57));
 
     }
 
---
+++ Lambda/src/FunctionApi/composition/FunComposition.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FunctionApi.composition;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+/**
+ *
+ * @author merttan
+ */
+public class FunComposition {
+
+    public static void compose() {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Lütfen bir rakam giriniz...");
+        int i = scn.nextInt();
+        
+        Function<Integer, Integer> twice= k -> k*2;
+        Function<Integer, Integer> square= k -> k*k;
+        
+        int twiceNumber= twice.apply(i);
+        int squareNumber=square.apply(i);
+        System.out.println(squareNumber);
+        System.out.println(twiceNumber);
+        
+    }
+
+    public static void main(String... args) {
+
+        
+        compose();
+    }
+
+}
